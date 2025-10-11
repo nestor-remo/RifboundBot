9de32a7ffd1393f10a8913f72fe6ff71a569336a
Nestor Reategui <nestiray12345@gmail.com>
2025-10-10 21:53:49 -0700

    quickstart

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import csv
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -12,21 +13,28 @@ response = requests.get(url)
 data = response.json()
 
 cards = data["items"]
-# print(data)
 
-with open("cards.csv", "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(["number", "id", "orientation", "title", "altText"])
+# with open("cards.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["number", "id", "orientation", "title", "altText"])
 
-    for card in cards:
-        writer.writerow([
-            card.get("number"),
-            card.get("id"),
-            card.get("orientation"),
-            card.get("title"),
-            card.get("altText"),
-        ])
+#     for card in cards:
+#         writer.writerow([
+#             card.get("number"),
+#             card.get("id"),
+#             card.get("orientation"),
+#             card.get("title"),
+#             card.get("altText"),
+#         ])
 
-print(cards)
+with open("cards.json", "w") as file:
+    json.dump(cards, file, indent=4)
+
+with open("cards.json", "r") as file:
+    cards = json.load(file)
+
+print(cards[:3])
+
+# print(cards)
 # for card in cards[:3]:
 #     print(f"{card.get('number')}: {card.get('title')} ({card.get('id')})")
