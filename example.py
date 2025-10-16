5c7d8e32e3a34fc4f3e4f78f7b18b2fafdedbe4c
Nestor Reategui <nestiray12345@gmail.com>
2025-10-15 23:49:03 -0700

    reorganized json for faster lookup, added command to fetch card information

--- example.py
+++ example.py
@@ -1,23 +1,60 @@
 import discord
+from discord.ext import commands
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
-@client.event
+
+
+CARD_DATA = {} 
+
+with open("cards_by_title.json", "r") as file:
+    CARD_DATA = json.load(file)
+# print(CARD_DATA)
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('shut up lil bro')
+    await bot.process_commands(message)
 
-client.run(os.getenv("DISCORD_BOT_TOKEN"))
+# @bot.event
+# async def on_typing(channel, user, when):
+#     await channel.send(f'buffoonery happening in {channel}' )
+#     await channel.send(f'I can smell you {user}...')
+#     await channel.send('stop typing')
+
+@bot.command()
+async def test(context, arg):
+    await context.send(f'This is a test command with argument: {arg}')
+
+
+@bot.command(name='card')
+async def get_card_info(context, *, card_name):
+    card_name = card_name
+    card_info = CARD_DATA.get(card_name)
+    if card_info:
+        info_lines = [f"**{card_name.title()}**"]
+        for key, value in card_info.items():
+            info_lines.append(f"**{key.capitalize()}**: {value}")
+        response = "\n".join(info_lines)
+    else:
+        response = f"Card '{card_name}' not found."
+    await context.send(response)
+
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
--- main.py
+++ main.py
@@ -14,27 +14,22 @@ data = response.json()
 
 cards = data["items"]
 
-# with open("cards.csv", "w", newline="") as file:
-#     writer = csv.writer(file)
-#     writer.writerow(["number", "id", "orientation", "title", "altText"])
-
-#     for card in cards:
-#         writer.writerow([
-#             card.get("number"),
-#             card.get("id"),
-#             card.get("orientation"),
-#             card.get("title"),
-#             card.get("altText"),
-#         ])
-
 with open("cards.json", "w") as file:
     json.dump(cards, file, indent=4)
 
 with open("cards.json", "r") as file:
     cards = json.load(file)
 
-print(cards[:3])
+cards_by_title = {}
 
-# print(cards)
-# for card in cards[:3]:
-#     print(f"{card.get('number')}: {card.get('title')} ({card.get('id')})")
+for card in cards:
+    title = card['title']
+    new_card = {}
+    for key, value in card.items():
+        if key != 'title':
+            new_card[key] = value
+
+    cards_by_title[title] = new_card
+
+with open("cards_by_title.json", "w") as file:
+    json.dump(cards_by_title, file, indent=4)
